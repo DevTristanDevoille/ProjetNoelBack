de2d6f513b0d096aa327ade2e23c1acef490f1bd
Tristan DEVOILLE <tristan.devoille@diiage.org>
2022-11-28 19:35:41 +0100

    Ajout nouvelle migration

--- ProjetNoelAPI/ProjetNoelAPI.DataAccess/Migrations/20221128183507_TestCICD.Designer.cs
+++ ProjetNoelAPI/ProjetNoelAPI.DataAccess/Migrations/20221128183507_TestCICD.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetNoelAPI.DataAccess.DbContextNoel;
 
@@ -11,9 +12,10 @@ using ProjetNoelAPI.DataAccess.DbContextNoel;
 namespace ProjetNoelAPI.DataAccess.Migrations
 {
     [DbContext(typeof(NoelDbContext))]
-    partial class NoelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221128183507_TestCICD")]
+    partial class TestCICD
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
