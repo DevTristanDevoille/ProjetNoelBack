95e6fb055f7da916d737d54f58e2423e42d0daa5
Tristan DEVOILLE <tristan.devoille@diiage.org>
2022-11-29 22:32:33 +0100

    Ajout Idea

---
+++ ProjetNoelAPI/ProjetNoelAPI.DataAccess/Migrations/20221129213206_AjoutOrdre.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetNoelAPI.DataAccess.Migrations
+{
+    public partial class AjoutOrdre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Position",
+                table: "Ideas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Ideas");
+        }
+    }
+}
