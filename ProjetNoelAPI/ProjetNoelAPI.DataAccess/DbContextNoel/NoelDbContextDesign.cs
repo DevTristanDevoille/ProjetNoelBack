1ee0e948facf903ac2ea75707d801fe0c01513b7
Tristan DEVOILLE <tristan.devoille@diiage.org>
2022-12-11 12:15:46 +0100

    Ajout du champ commentaire

--- ProjetNoelAPI/ProjetNoelAPI.DataAccess/DbContextNoel/NoelDbContextDesign.cs
+++ ProjetNoelAPI/ProjetNoelAPI.DataAccess/DbContextNoel/NoelDbContextDesign.cs
@@ -9,7 +9,7 @@ namespace ProjetNoelAPI.DataAccess.DbContextNoel
         {
             var optionsBuilder = new DbContextOptionsBuilder<NoelDbContext>();
 
-            string connectionString = "Server=tcp:projetnoelsql.database.windows.net,1433;Initial Catalog=ProjetNoelDb;Persist Security Info=False;User ID=superadmin;Password=Azerty@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            string connectionString = "";
 
             optionsBuilder.UseSqlServer(connectionString);
             return new NoelDbContext(optionsBuilder.Options);
